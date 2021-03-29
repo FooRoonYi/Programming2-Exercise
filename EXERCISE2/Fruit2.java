7a8d8225b82c956b2340ea394d22548a8f1ba77f
Foo Roon Yi <80664992+FooRoonYi@users.noreply.github.com>
2021-03-29 20:54:28 +0800

    Add files via upload

---
+++ EXERCISE2/Fruit2.java
@@ -0,0 +1,28 @@
+package EXERCISE2;
+
+public class Fruit2 {
+
+	String c = "green", mC = "important minerals and vitamins", a = "rough", func;
+	Boolean f = true;
+	
+	void printColour() {
+		System.out.println(("colour = " + c);
+	}
+	
+	void printMainComponents() {
+		System.out.println("Fruits contain " + mC);
+	}
+	
+	void printFragrance() {
+		System.out.println("Does the fruit have fragrance?: " + f);
+	}
+	
+	void printAppearance() {
+		System.out.println("The appearance of the fruits is " + a);
+	}
+	
+	void printFunc() {
+		func = "provide vitamins, reduce risk of heart disease, prevent constipation, provide health benefits";
+		System.out.println("The pineapples can " + func + ".");
+	}
+}
